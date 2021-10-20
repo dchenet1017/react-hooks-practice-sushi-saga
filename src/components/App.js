af26b786ae30c6af2f5d6958782f898d96e92d51
Dimitri Chenet <dchenet1017@gmail.com>
2021-10-20 19:28:15 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
 import React from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const API = "http://localhost:3001/sushis";
 
+
 function App() {
+
+    const [sushifood, setsushifood]=useState([])
+
+    useEffect(() => {
+      fetch(API)
+      .then(r => r.json())
+      .then(data => setsushifood(data))
+    },[])
+    
+console.log(sushifood,"first")
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={sushifood} />
       <Table />
     </div>
   );
+  
 }
+
+
 
 export default App;
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,10 +1,13 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushis}) {
+  console.log(sushis,"second")
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {sushis.map(sushi => <Sushi sushi={sushi} />)}
+      
       <MoreButton />
     </div>
   );
